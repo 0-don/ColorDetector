b74a30309107843377f7c8dae7b5bcaa86f587e1
Don-Cryptus <don.cryptus@gmail.com>
2022-09-24 16:24:40 +0200

    add fxml

--- src/main/java/com/changenode/BaseApplication.java
+++ src/main/java/com/changenode/BaseApplication.java
@@ -2,6 +2,7 @@ package com.changenode;
 
 import com.changenode.plugin.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -32,14 +33,14 @@ public class BaseApplication extends Application implements Log {
         /*
          * Route the debugging output for this application to a log file in your "default" directory.
          * */
-        try {
-            outputFile = File.createTempFile("debug", ".log", getFileSystemView().getDefaultDirectory());
-            PrintStream output = new PrintStream(new BufferedOutputStream(new FileOutputStream(outputFile)), true);
-            System.setOut(output);
-            System.setErr(output);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            outputFile = File.createTempFile("debug", ".log", getFileSystemView().getDefaultDirectory());
+//            PrintStream output = new PrintStream(new BufferedOutputStream(new FileOutputStream(outputFile)), true);
+//            System.setOut(output);
+//            System.setErr(output);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         launch(args);
     }
@@ -51,54 +52,12 @@ public class BaseApplication extends Application implements Log {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
 
-        BorderPane borderPane = new BorderPane();
-
-        VBox topElements = new VBox();
-
-        MenuBar menuBar = new MenuBar();
-        topElements.getChildren().add(menuBar);
-
-        ToolBar toolbar = new ToolBar();
-        topElements.getChildren().add(toolbar);
-
-        textArea = new TextArea();
-        textArea.setWrapText(true);
-
-        statusLabel = new Label();
-        statusLabel.setPadding(new Insets(5.0f, 5.0f, 5.0f, 5.0f));
-        statusLabel.setMaxWidth(Double.MAX_VALUE);
-
-        borderPane.setTop(topElements);
-        borderPane.setBottom(statusLabel);
-        borderPane.setCenter(textArea);
-
-        Scene scene = new Scene(borderPane, 800, 600);
-
-        stage.setTitle("Hello World");
+        FXMLLoader fxmlLoader = new FXMLLoader( getClass().getClassLoader().getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
-
-        for (Plugin plugin : plugins) {
-            try {
-                plugin.setup(stage, textArea, toolbar, this, menuBar);
-            } catch (Exception e) {
-                System.err.println("Unable to start plugin");
-                System.err.println(plugin.getClass().getName());
-                e.printStackTrace();
-                log("Unable to start plugin");
-                log(plugin.getClass().getName());
-                log(e.getMessage());
-            }
-        }
-
-        statusLabel.setText("Ready.");
-
         stage.show();
-        //put window to front to avoid it to be hide behind other.
-        stage.setAlwaysOnTop(true);
-        stage.requestFocus();
-        stage.toFront();
-        stage.setAlwaysOnTop(false);
     }
 }
