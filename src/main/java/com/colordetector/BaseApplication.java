84ab258d772a3df7547a5827f28f726907f16606
Don-Cryptus <don.cryptus@gmail.com>
2022-09-25 15:49:11 +0200

    add controller ids

--- src/main/java/com/colordetector/BaseApplication.java
+++ src/main/java/com/colordetector/BaseApplication.java
@@ -1,7 +1,9 @@
 package com.colordetector;
 
+import com.colordetector.controller.ColorDetectorController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -29,10 +31,14 @@ public class BaseApplication extends Application {
     public void start(Stage stage) throws IOException, AWTException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = fxmlLoader.load();
+        ColorDetectorController controller = fxmlLoader.getController();
+
+        Scene scene = new Scene(root);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        controller.setX("wtf");
 
 //        Robot robot = new Robot();
 //
--- src/main/java/com/colordetector/controller/ColorDetectorController.java
+++ src/main/java/com/colordetector/controller/ColorDetectorController.java
@@ -28,5 +28,9 @@ public class ColorDetectorController {
 
     @FXML
     private Pane color;
+
+    public void setX(String text) {
+        x.setText(text);
+    }
 }
 
