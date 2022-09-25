3af4e1effd8e82db8cbec29cf48b148d0e3eddae
Don-Cryptus <don.cryptus@gmail.com>
2022-09-25 22:10:45 +0200

    finished

--- src/main/java/com/colordetector/BaseApplication.java
+++ src/main/java/com/colordetector/BaseApplication.java
@@ -1,33 +1,31 @@
 package com.colordetector;
 
 import com.colordetector.controller.ColorDetectorController;
-import com.colordetector.utils.ColorUtils;
 import com.colordetector.utils.GlobalKeyListener;
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import java.io.IOException;
+import java.io.*;
+
+import static javax.swing.filechooser.FileSystemView.getFileSystemView;
 
 public class BaseApplication extends Application {
 
     public static void main(String[] args) {
 
-//        try {
-//            File outputFile = File.createTempFile("debug", ".log", getFileSystemView().getDefaultDirectory());
-//            PrintStream output = new PrintStream(new BufferedOutputStream(new FileOutputStream(outputFile)), true);
-//            System.setOut(output);
-//            System.setErr(output);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            File outputFile = File.createTempFile("debug", ".log", getFileSystemView().getDefaultDirectory());
+            PrintStream output = new PrintStream(new BufferedOutputStream(new FileOutputStream(outputFile)), true);
+            System.setOut(output);
+            System.setErr(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         launch(args);
 
@@ -40,32 +38,6 @@ public class BaseApplication extends Application {
         Parent root = fxmlLoader.load();
         ColorDetectorController controller = fxmlLoader.getController();
 
-        Task<Void> task = new Task<Void>() {
-            @Override
-            protected Void call() throws Exception {
-                Robot robot = new Robot();
-                ColorUtils colorUtils = new ColorUtils();
-                while (true) {
-                    Point cord = MouseInfo.getPointerInfo().getLocation();
-                    Color color = robot.getPixelColor((int) cord.getX(), (int) cord.getY());
-                    String hex = "#" + Integer.toHexString(color.getRGB()).substring(2).toUpperCase();
-
-                    Platform.runLater(() -> {
-                        controller.getX().setText(String.valueOf((int) cord.getX()));
-                        controller.getY().setText(String.valueOf((int) cord.getY()));
-                        controller.getRed().setText(String.valueOf(color.getRed()));
-                        controller.getBlue().setText(String.valueOf(color.getBlue()));
-                        controller.getGreen().setText(String.valueOf(color.getGreen()));
-                        controller.getHex().setText(hex);
-                        controller.getName().setText(colorUtils.getColorNameFromColor(color));
-                        controller.getColor().setStyle("-fx-background-color: " + hex + "; " + "-fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-style: solid; -fx-border-width: 0.5px; -fx-border-radius: 5px;");
-                    });
-                    robot.delay(10);
-                }
-            }
-        };
-        new Thread(task).start();
-
 
         Scene scene = new Scene(root);
         stage.setTitle("Color Detector");
@@ -74,7 +46,7 @@ public class BaseApplication extends Application {
 
 
         GlobalScreen.registerNativeHook();
-        GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
+        GlobalScreen.addNativeKeyListener(new GlobalKeyListener(controller));
     }
 
 
--- src/main/java/com/colordetector/utils/GlobalKeyListener.java
+++ src/main/java/com/colordetector/utils/GlobalKeyListener.java
@@ -1,13 +1,57 @@
 package com.colordetector.utils;
 
+import com.colordetector.controller.ColorDetectorController;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import lombok.RequiredArgsConstructor;
 
+import java.awt.*;
+
+@RequiredArgsConstructor
 public class GlobalKeyListener implements NativeKeyListener {
+    private final ColorDetectorController controller;
+
+    private boolean isRunning = false;
 
     public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-//        GlobalScreen.unregisterNativeHook();
 
+//        GlobalScreen.unregisterNativeHook();
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            isRunning = !isRunning;
+            if (isRunning) startDetecting();
+        }
+    }
+
+    public void startDetecting() {
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                Robot robot = new Robot();
+                ColorUtils colorUtils = new ColorUtils();
+                while (isRunning) {
+                    Point cord = MouseInfo.getPointerInfo().getLocation();
+                    Color color = robot.getPixelColor((int) cord.getX(), (int) cord.getY());
+                    String hex = "#" + Integer.toHexString(color.getRGB()).substring(2).toUpperCase();
+
+                    Platform.runLater(() -> {
+                        controller.getX().setText(String.valueOf((int) cord.getX()));
+                        controller.getY().setText(String.valueOf((int) cord.getY()));
+                        controller.getRed().setText(String.valueOf(color.getRed()));
+                        controller.getBlue().setText(String.valueOf(color.getBlue()));
+                        controller.getGreen().setText(String.valueOf(color.getGreen()));
+                        controller.getHex().setText(hex);
+                        controller.getName().setText(colorUtils.getColorNameFromColor(color));
+                        controller.getColor().setStyle("-fx-background-color: " + hex + "; " + "-fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-style: solid; -fx-border-width: 0.5px; -fx-border-radius: 5px;");
+                    });
+                    robot.delay(10);
+
+                }
+                return null;
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
     }
 }
