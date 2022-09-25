3b840f0ee0fa1f13d055645fe7d7e0b02616ea84
Don-Cryptus <don.cryptus@gmail.com>
2022-09-25 16:48:18 +0200

    fill out all fields

--- src/main/java/com/colordetector/BaseApplication.java
+++ src/main/java/com/colordetector/BaseApplication.java
@@ -1,6 +1,7 @@
 package com.colordetector;
 
 import com.colordetector.controller.ColorDetectorController;
+import com.colordetector.utils.ColorUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -28,7 +29,7 @@ public class BaseApplication extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException, AWTException {
+    public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("view.fxml"));
         Parent root = fxmlLoader.load();
@@ -38,15 +39,35 @@ public class BaseApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        controller.getX().setText("wtf");
 
-//        Robot robot = new Robot();
+
+        new Thread(() -> {
+
+            while (true) {
+                try {
+                    Robot robot = new Robot();
+                    ColorUtils colorUtils = new ColorUtils();
+                    Point cord = MouseInfo.getPointerInfo().getLocation();
+                    Color color = robot.getPixelColor((int) cord.getX(), (int) cord.getY());
+                    String hex = "#" + Integer.toHexString(color.getRGB()).substring(2).toUpperCase();
+                    controller.getX().setText(String.valueOf((int) cord.getX()));
+                    controller.getY().setText(String.valueOf((int) cord.getY()));
+                    controller.getRed().setText(String.valueOf(color.getRed()));
+                    controller.getBlue().setText(String.valueOf(color.getBlue()));
+                    controller.getGreen().setText(String.valueOf(color.getGreen()));
+                    controller.getHex().setText(hex);
+                    controller.getName().setText(colorUtils.getColorNameFromColor(color));
+
+                    controller.getColor().setStyle("-fx-background-color: " + hex + "; " + "-fx-text-fill: #ffffff; -fx-border-color: #ffffff; -fx-border-style: solid; -fx-border-width: 0.5px; -fx-border-radius: 5px;");
+
+                    robot.delay(1000);
+                } catch (Throwable ignored) {
+                }
+            }
+        }).start();
+
+
 //
-//        while (true) {
-//            Point cord = MouseInfo.getPointerInfo().getLocation();
-//            Color color = robot.getPixelColor((int) cord.getX(), (int) cord.getY());
-//            System.out.println("X: " + cord.getX() + " Y: " + cord.getY() + " Color: " + color);
-//            robot.delay(1000);
-//        }
+
     }
 }
