6b62520200f80b1588bbf43f1e46fce2d7f4a7e9
Don-Cryptus <don.cryptus@gmail.com>
2022-10-18 14:11:10 +0200

    add logo

--- src/main/java/com/colordetector/BaseApplication.java
+++ src/main/java/com/colordetector/BaseApplication.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -60,6 +61,7 @@ public class BaseApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
+        stage.getIcons().add(new Image("file:app-icon.png"));
 
         GlobalScreen.registerNativeHook();
         GlobalKeyListener globalKeyListener = new GlobalKeyListener(controller);
